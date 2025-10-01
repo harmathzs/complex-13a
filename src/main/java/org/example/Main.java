da9e052ee1179eac6d454a9a04976180cfe02253
Zsolt Harmath <harmathzs@gmail.com>
2025-10-01 12:43:02 +0200

    Calculate absolute value of complex number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -63,6 +63,14 @@ public class Main {
                 break;
             }
             case '4' : {
+                System.out.print("z: ");
+                String aStr = input.nextLine();
+                Complex z = new Complex(aStr);
+
+                double abs = z.abs();
+
+                System.out.println("|"+z+"| = "+abs);
+
                 break;
             }
             case '5' : {
